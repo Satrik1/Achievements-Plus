6597671b4dcbd67a140ad661e4264f8b756b842e
nqmashnet <codecommander8@icloud.com>
2026-01-07 22:00:32 +0200

    Delete source-code directory

--- source-code/src/Hooks/CCTouchDispatcher.hpp
+++
@@ -1,9 +0,0 @@
-#include "../Includes.hpp"
-
-#include <Geode/modify/CCTouchDispatcher.hpp>
-
-class $modify(ProCCTouchDispatcher, CCTouchDispatcher) {
-
-    void touches(CCSet* p0, CCEvent* p1, unsigned int p2);
-
-};
